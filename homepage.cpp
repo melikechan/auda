732b632a6a972cf7fe34f091a7e480a203cede34
melikechan <melikechan@tuta.io>
2021-10-20 22:35:37 +0300

    Added initial design

---
+++ homepage.cpp
@@ -0,0 +1,15 @@
+#include "homepage.h"
+#include "ui_homepage.h"
+
+HomePage::HomePage(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::HomePage)
+{
+    ui->setupUi(this);
+}
+
+HomePage::~HomePage()
+{
+    delete ui;
+}
+
---
+++ homepage.h
@@ -0,0 +1,21 @@
+#ifndef HOMEPAGE_H
+#define HOMEPAGE_H
+
+#include <QMainWindow>
+
+namespace Ui { class HomePage; }
+
+
+class HomePage : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit HomePage(QWidget *parent = nullptr);
+    ~HomePage();
+
+private:
+    Ui::HomePage *ui;
+};
+
+#endif // HOMEPAGE_H
